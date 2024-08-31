c9906d5bc94b54dd079bafb28470b5788bb36327
PatrickPT24 <161574851+PatrickPT24@users.noreply.github.com>
2024-08-31 23:06:00 +0530

    Initial_Commit_01

---
+++ lscript.js
@@ -0,0 +1,23 @@
+let menuToggle = document.querySelector('.menuToggle');
+let lightDark = document.querySelector('.lightDark');
+let body = document.querySelector('body');
+let video = document.querySelector('video');
+let dark = false;
+let navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+};
+
+lightDark.onclick = function(){
+    dark = !dark;
+    lightDark.classList.toggle('active');
+    body.classList.toggle('dark');
+
+    if (dark) {
+        video.setAttribute('src', "/Prodigy_Infotech/WD_01/Media/nightVideo_02.mp4");
+        } else {
+            video.setAttribute('src', "/Prodigy_Infotech/WD_01/Media/dayVideo_01.mp4");
+    }
+};
