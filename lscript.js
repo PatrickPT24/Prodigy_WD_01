3e0f0e94e7c2b1b68d796403264fdb8135f4d3fe
PatrickPT24 <161574851+PatrickPT24@users.noreply.github.com>
2024-09-01 01:18:18 +0530

    Updated_image_and bgVideo paths

--- lscript.js
+++ lscript.js
@@ -16,7 +16,7 @@ lightDark.onclick = function(){
     body.classList.toggle('dark');
 
     if (dark) {
-        video.setAttribute('src', "C:\P@trick\C Pgms\Practice Projects\Prodigy_WD_01/Media/nightVideo_02.mp4");
+        video.setAttribute('src', "/Prodigy_WD_01/Media/nightVideo_02.mp4");
         } else {
             video.setAttribute('src', "/Prodigy_Infotech/WD_01/Media/dayVideo_01.mp4");
     }
